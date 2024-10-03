e937b68d84239d089ffc0b6612802e030d2756e2
Yulia393 <zakharovajulia393@gmail.com>
2024-10-03 19:51:03 +0300

    Add files via upload

---
+++ module_1_3.1.py
@@ -0,0 +1,5 @@
+home_tasks = 12
+work_hours = 1.5
+course_name = "Python"
+time_spent = work_hours / home_tasks
+print("Курс:", course_name, ", всего задач:", home_tasks, ", затрачено часов:", work_hours, ", среднее время выполнения", time_spent, "часа")
---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name = "Yulia"
+print(name)
+age = 35
+print(age)
+age = age + 20
+print(age)
+is_student = name
+print(bool(is_student))
